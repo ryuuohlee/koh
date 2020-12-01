1ab8db763b6c9735bb10010afbadf78b9d467426
Jeffrey Lee <jglee1@live.com>
2020-11-30 22:57:49 -0800

    Made animated background and added a Rank number

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from '../components/Navigation/Navigation.jsx';
 import Logo from '../components/Logo/Logo.jsx';
+import Rank from '../components/Rank/Rank.jsx';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm.jsx';
 import './App.css';
 import 'tachyons';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    },
+    color: "#242424",
+    line_linked: {
+      color: "#242424"
+    }
+  }
+}
 
 class App extends React.Component {
   render() {
     return(
       <div className='App'>
+         <Particles className="particles"
+          params={particlesOptions}
+         />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
        {/* {
 
