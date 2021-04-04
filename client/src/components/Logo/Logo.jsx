586e30e84af46c09701d31d39cbb0ff18c65e565
Jeffrey Lee <jglee1@live.com>
2021-04-03 21:56:17 -0700

    Feature: optimize

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import logo from '../../reminded-me-of-koh-the-face-stealer-47674503.png';
 import './Logo.css';
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
       <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-        <div className="Tilt-inner"><img alt="logo" src={logo}></img></div>
+        <div className="Tilt-inner"><img alt="logo" src="https://jeffreyleeportfolio.s3-us-west-1.amazonaws.com/reminded-me-of-koh-the-face-stealer-47674503.png"></img></div>
       </Tilt>
     </div>
   )
